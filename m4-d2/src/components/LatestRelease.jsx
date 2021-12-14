e2662144add3e5e2f97acc8fb3f9ba56e99d336e
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-12-14 17:31:01 +0100

    Col issue fixed

--- m4-d2/src/components/LatestRelease.jsx
+++ m4-d2/src/components/LatestRelease.jsx
@@ -1,22 +1,22 @@
 import { Container, Row, Col, Card } from 'react-bootstrap'
-import dishes from '../data/history.json'
+import books from '../data/history.json'
 
 const LatestRelease = () => {
     return (
         <Container>
             <Row className="justify-content-around">
-                {/* <Col xs={12} md={6} lg={3}> */}
-                    {
-                        dishes.map(book => (
-                            <Card key={book.asin} className="my-3 mx-3 col-6 col-md-3 col-lg-2">
-                                <Card.Img className="img-fluid w-100" variant="top" src={book.img}/>
+                {
+                    books.map(book => (
+                        <Col xs={12} sm={6} md={4} lg={3}>
+                            <Card key={book.asin} className="my-3 mx-3">
+                                <Card.Img className="img-fluid w-100" variant="top" src={book.img} />
                                 <Card.Body>
                                     <Card.Title id='book-title'><small>{book.title}</small></Card.Title>
                                 </Card.Body>
                             </Card>
-                        ))
-                    }
-                {/* </Col> */}
+                        </Col>
+                    ))
+                }
             </Row>
         </Container>
     )
