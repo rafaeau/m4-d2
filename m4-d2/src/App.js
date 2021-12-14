5c200a2161fa2161833d4457aefd2da7be8b5df2
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-12-14 14:50:40 +0100

    MyFooter component

--- m4-d2/src/App.js
+++ m4-d2/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import MyNav from './components/MyNav'
+import MyFooter from './components/MyFooter';
 
 function App() {
   return (
     <div className="App">
       <MyNav/>
+      <MyFooter/>
     </div>
   );
 }
---
+++ m4-d2/src/components/MyFooter.jsx
@@ -0,0 +1,19 @@
+const MyFooter = () => {
+    return (
+        <footer className="page-footer font-small bg-light">
+            <div className="container-fluid text-center">
+                <div className="row">
+                    <div className="col-md-12 mt-1">
+                        <h5>Footer Content</h5>
+                        <p>Here you can use rows and columns to organize your footer content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="footer-copyright text-center mt-n2 pb-3">© 2021 Copyright: Rafael Ferreira</div>
+
+        </footer>
+    )
+}
+
+export default MyFooter
